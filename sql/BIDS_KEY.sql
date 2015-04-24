96509899e482c60ff01b27c179a71ec5de2a4f48
n.dobryukha <n.dobryukha@gmail.com>
2015-04-24 16:37:58 +0300

    add sql scripts

---
+++ sql/BIDS_KEY.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Friday-April-24-2015   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence BIDS_KEY
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "MARKETPLACE"."BIDS_KEY"  MINVALUE 1 MAXVALUE 9999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE ;
---
+++ sql/ITEMS_KEY.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Friday-April-24-2015   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence ITEMS_KEY
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "MARKETPLACE"."ITEMS_KEY"  MINVALUE 1 MAXVALUE 9999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE ;
