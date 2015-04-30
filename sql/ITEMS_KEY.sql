c088e5d5fa20c7aa50b9cb653e01bf0f953d9523
n.dobryukha <n.dobryukha@gmail.com>
2015-04-30 15:21:43 +0300

    delete old sql scripts

--- sql/ITEMS_KEY.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------
---  File created - Friday-April-24-2015   
---------------------------------------------------------
---------------------------------------------------------
---  DDL for Sequence ITEMS_KEY
---------------------------------------------------------
-
-   CREATE SEQUENCE  "MARKETPLACE"."ITEMS_KEY"  MINVALUE 1 MAXVALUE 9999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE ;
--- sql/USERS_KEY.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------
---  File created - Friday-April-24-2015   
---------------------------------------------------------
---------------------------------------------------------
---  DDL for Sequence USERS_KEY
---------------------------------------------------------
-
-   CREATE SEQUENCE  "MARKETPLACE"."USERS_KEY"  MINVALUE 1 MAXVALUE 9999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE ;
