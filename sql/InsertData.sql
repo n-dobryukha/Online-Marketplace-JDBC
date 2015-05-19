29abd75561c95f0c7c5a18c7f8d5b91767d39338
n.dobryukha <n.dobryukha@gmail.com>
2015-05-19 17:46:23 +0300

    update sql

--- sql/InsertData.sql
+++ sql/InsertData.sql
@@ -1,7 +1,6 @@
-INSERT INTO Users (fullName, billingAddress, login, password, email) VALUES('Иванов Иван', 'г. Саратов, ул. Рахова, д. 181', 'i.ivanov', 'password01', 'i.ivanov@example.com');
-INSERT INTO Users (fullName, billingAddress, login, password, email) VALUES('Петров Пётр', 'г. Саратов, ул. Киселева, д. 40', 'p.petrov', 'password02', 'p.petrov@example.com');
-INSERT INTO Users (fullName, billingAddress, login, password, email) VALUES('Сергеев Сергей', 'г. Саратов, ул. Московская, д. 10', 's.sergeev', 'password03', 's.sergeev@example.com');
-INSERT INTO Users (fullName, billingAddress, login, password, email) VALUES('Николаев Николай', 'г. Саратов, ул. Чапаева, д. 131', 'n.nikolaev', 'password04', 'n.nikolaev@example.com');
-INSERT INTO Users (fullName, billingAddress, login, password, email) VALUES('Антонов Антон', 'г. Саратов, ул. Университетская, д. 28', 'a.antonov', 'password05', 'a.antonov@example.com');
-
+INSERT INTO USERS (FULL_NAME, BILLING_ADDRESS, LOGIN, PASSWORD, EMAIL) VALUES('Иванов Иван', 'г. Саратов, ул. Рахова, д. 181', 'i.ivanov', 'PASSWORD01', 'i.ivanov@example.com');
+INSERT INTO USERS (FULL_NAME, BILLING_ADDRESS, LOGIN, PASSWORD, EMAIL) VALUES('Петров Пётр', 'г. Саратов, ул. Киселева, д. 40', 'p.petrov', 'PASSWORD02', 'p.petrov@example.com');
+INSERT INTO USERS (FULL_NAME, BILLING_ADDRESS, LOGIN, PASSWORD, EMAIL) VALUES('Сергеев Сергей', 'г. Саратов, ул. Московская, д. 10', 's.sergeev', 'PASSWORD03', 's.sergeev@example.com');
+INSERT INTO USERS (FULL_NAME, BILLING_ADDRESS, LOGIN, PASSWORD, EMAIL) VALUES('Николаев Николай', 'г. Саратов, ул. Чапаева, д. 131', 'n.nikolaev', 'PASSWORD04', 'n.nikolaev@example.com');
+INSERT INTO USERS (FULL_NAME, BILLING_ADDRESS, LOGIN, PASSWORD, EMAIL) VALUES('Антонов Антон', 'г. Саратов, ул. Университетская, д. 28', 'a.antonov', 'PASSWORD05', 'a.antonov@example.com');
 COMMIT;
